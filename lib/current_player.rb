3713777a073133e1bb565049a24d1fa141a1c324
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-03-03 05:21:34 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count (board)
+  turncount = 0
+  board.each do |spaces|
+   if spaces != " "
+     turncount += 1
+   end
+  end
+  return turn_count(board).length
+end
+
+
+
+def current_player
+  
+end
