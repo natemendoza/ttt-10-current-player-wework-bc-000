85c202c89dd0d2ec932462aa53c8d6006ca29271
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-03-03 05:35:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,8 +11,9 @@ end
 def current_player (board)
   turn_count (board)
     if turn_count (board) !% 2 == 1
-    return "X"
-  else
-    return "O"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
