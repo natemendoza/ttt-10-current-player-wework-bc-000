bb61d53b847fa6eacfd6655acbfdd04b58b1e3e4
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-03-03 05:56:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,7 +12,7 @@ def current_player (board)
   result = turn_count (board)
   #result is using board array values 0-8 so I'm adding +1
   result += 1
-  if result.odd
+  if result.odd?
     return "X"
   else
     return "O"
